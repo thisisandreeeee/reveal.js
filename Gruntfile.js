8ca5b096ceb41808e038f7d8eb65a6ba3209fc4c
Andre Tan <tanandre93@gmail.com>
2016-02-01 18:05:50 +0800

    Update file structures

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,6 @@ module.exports = function(grunt) {
 	// Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			banner:
-				'/*!\n' +
-				' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-				' * http://lab.hakim.se/reveal-js\n' +
-				' * MIT licensed\n' +
-				' *\n' +
-				' * Copyright (C) 2016 Hakim El Hattab, http://hakim.se\n' +
-				' */'
-		},
 
 		qunit: {
 			files: [ 'test/*.html' ]
@@ -129,7 +119,7 @@ module.exports = function(grunt) {
 				tasks: 'css-core'
 			},
 			html: {
-				files: [ 'index.html']
+				files: [ 'index.html','USE2317_Teh.html']
 			},
 			markdown: {
 				files: [ './*.md' ]
